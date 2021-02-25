3a17420cee1b98c6e70c81b0468720b8484fc850
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-02-25 10:59:00 +0900

    画像保存用設定(active_storage)

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,4 +3,11 @@ class PrototypesController < ApplicationController
   def index
 
   end
+
+  private
+
+  # 許可するパラメータを指定（imageは画像用）
+  def prototype_params
+    params.require(:prototype).permit(:title, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,6 +4,9 @@ class Prototype < ApplicationRecord
   validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
+  validates :image, presence: true
   # アソシエーション
   belongs_to :user
+  # image登録用
+  has_one_attached :image
 end
